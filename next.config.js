5e9d1c0ff1b9bfff6408e25b4b82d26b591e25f7
DeccaSoft <andremazevedo@gmail.com>
2022-12-15 14:22:17 -0300

    ajustando...

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePahts: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <h1>Olá Mundo!</h1>
+  )
+}
